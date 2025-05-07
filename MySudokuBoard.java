31c7147a4f824ad6a559de433cfd2e84610e21c7
Percy0506 <ngyuisik@gmail.com>
2025-05-07 12:14:26 -0700

    Add files via upload

--- MySudokuBoard.java
+++ MySudokuBoard.java
@@ -1,130 +1,200 @@
-/*
-   This is starter code for Crystal's Sudoku #3.
-   The code is not pretty, but it works.
-*/
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
-import java.io.*;
+
 
 public class MySudokuBoard {
-   public final int SIZE = 9;
-   protected char[][] myBoard;
-   
-   public MySudokuBoard(String theFile) {
-      myBoard = new char[SIZE][SIZE];
-      try {
-         Scanner file = new Scanner(new File(theFile));
-         for(int row = 0; row < SIZE; row++) {
-            String theLine = file.nextLine();
-            for(int col = 0; col < theLine.length(); col++) {
-               myBoard[row][col] = theLine.charAt(col);
+
+    private int[][] board;
+    
+    public MySudokuBoard(String filename) {
+        board = new int[9][9];
+        try {
+            Scanner file = new Scanner(new File(filename));
+            for (int r = 0; r < 9; r++) {
+                String line = file.nextLine();
+                for (int c = 0; c < 9; c++) {
+                    char ch = line.charAt(c);
+                    if (ch == '.') {
+                        board[r][c] = 0;
+                    } else {
+                       board[r][c] = ch - '0';
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot find file: " + filename);
+        }
+    }
+
+    public String toString() {
+      String result = "";
+      for(int r = 0; r < board.length; r++) {
+         for(int c = 0; c < board[0].length; c++) {
+            if(board[r][c] == 0) {
+               result += "X ";   
+            } else {
+               result += board[r][c] + " ";
             }
          }
-      } catch(Exception e) {
-         System.out.println("Something went wrong :(");
-         e.printStackTrace();
+         result += "\n";
       }
-   }
-   
-   public boolean isSolved() {
-      if(!isValid())
-         return false;
-         
-      Map<Character,Integer> map = new HashMap<>();
-      for(char[] row : myBoard) {
-         for(char cell : row) {
-            if(map.containsKey(cell))
-               map.put(cell, map.get(cell) + 1);
-            else 
-               map.put(cell, 1);
+
+      return result;
+    }
+    
+    
+    
+    public boolean isValid() {
+      return checkRows() && checkColumns() && checkMiniSquares() && checkWeriedChar(); 
+      //checkRows();
+      //checkRows() && checkColumns() && checkMiniSquares();
+    }
+    
+    private boolean checkWeriedChar() {
+      for(int r = 0; r < 9; r++) {
+         for(int c = 0; c < 9; c++) {
+            if(board[r][c] < 0 || board[r][c] > 9) { //track for weried char: &, @, #
+               return false;
+            }
          }
       }
-      // info on Collections: https://docs.oracle.com/javase/8/docs/api/?java/util/Collections.html
-      return map.keySet().size() == 9 && Collections.frequency(map.values(),9) == 9;
-   }
-   
-   public boolean isValid() {
-      // checks for bad data
-      for(char[] row : myBoard) 
-         for(char cell : row)
-            if(cell != '.' && (cell < '1' || cell > '9'))
-               return false;
-      
-      // checks for row/col violations
-      for(int r = 0; r < myBoard.length; r++) {
-         Set<Character> trackingRow = new HashSet<>();
-         Set<Character> trackingCol = new HashSet<>();
-         for(int c = 0; c < myBoard[r].length; c++) {
-            // check for row violation
-            if(trackingRow.contains(myBoard[r][c]))
-               return false;
-            else if(myBoard[r][c] != '.')
-               trackingRow.add(myBoard[r][c]);
-               
-            // check for col violation
-            if(trackingCol.contains(myBoard[c][r]))
-               return false;
-            else if(myBoard[c][r] != '.')
-               trackingCol.add(myBoard[c][r]);
-         } 
+      return true;
+    }
+    
+    //tracking for the role 
+    private boolean checkRows() {      
+      for(int r = 0; r < 9; r++){
+         Set<Integer> duplicate = new HashSet<>();
+         for(int c = 0; c < 9; c++){             
+            if(board[r][c] != 0 && duplicate.contains(board[r][c])) {
+               return false;            
+            } else {            
+               duplicate.add(board[r][c]);
+            }            
+         }
       }
+      return true;
+    }
       
-      // check for mini-squares
-      for(int square = 1; square <= 9; square++) {   
-         char[][] mini = miniSquare(square);
-         Set<Character> trackingMini = new HashSet<>();
-         for(int r = 0; r < 3; r++)
-            for(int c = 0; c < 3; c++)
-               // check for mini violation
-               if(trackingMini.contains(mini[r][c]))
-                  return false;
-               else if(mini[r][c] != '.')
-                  trackingMini.add(mini[r][c]);
+      
+   private boolean checkColumns() {       
+      for(int c = 0; c < 9; c++) {
+         Set<Integer> duplicate = new HashSet<>();
+         for(int r = 0; r < 9; r++) {          
+           if (board[r][c] != 0 && duplicate.contains(board[r][c])) {
+               return false;
+            } else {
+               duplicate.add(board[r][c]);
+            }           
+         }
       }
-      
-      // if there weren't any violations above...
       return true;
    }
    
-  private char[][] miniSquare(int spot) {
-      char[][] mini = new char[3][3];
+   private int[][] miniSquare(int spot) {
+      int[][] mini = new int[3][3];
       for(int r = 0; r < 3; r++) {
          for(int c = 0; c < 3; c++) {
-            // whoa - wild! This took me a solid hour to figure out.
+            // whoa - wild! This took me a solid hour to figure out (at least)
             // This translates between the "spot" in the 9x9 Sudoku board
             // and a new mini square of 3x3
-            mini[r][c] = myBoard[(spot - 1) / 3 * 3 + r][(spot - 1) % 3 * 3 + c];
+            mini[r][c] = board[(spot - 1) / 3 * 3 + r][(spot - 1) % 3 * 3 + c];
          }
       }
       return mini;
    }
    
-   //Solve method
-   public boolean solve(){
-      //base case 1, if it's not a valid board it can't be solved
-      if(!this.isValid()){
+   private boolean checkMiniSquares() {
+      for(int sqNum = 1; sqNum <= 9; sqNum++) {
+         int[][] mini = miniSquare(sqNum);
+         Set<Integer> duplicate = new HashSet<>();
+         for(int r = 0; r < 3; r++) {            
+            for(int c = 0 ; c < 3; c++) {  
+               int val = mini[r][c];            
+               if(val != 0 && duplicate.contains(val)) {
+                  return false;
+               } else {
+                  duplicate.add(val);
+               }
+            }
+         }
+      }
+      return true;
+   }
+ 
+   public boolean isSolved() {
+      if(!isValid()) {
          return false;
       }
-      //base case 2, if it is solved, return true
-      else if(this.isSolved()){
-         return true;
+      Map<Integer, Integer> map = new HashMap<>(); // <the elemet in the board, it's count>
+      for(int[] row : board) {
+         for(int num : row) {
+            if(num != 0) { //go foward if it's not blank
+               if(map.containsKey(num)) {
+                  map.put(num, map.get(num) + 1);                  
+               } else {
+                  map.put(num, 1);  //add num if it's not counted yet
+               }
+            } else {
+               return false; // return false if there's blank
+            }
+         }
       }
-      //recursive case (...)
-      else{
-         //temporary return
-         return false;
+      
+      for(Integer count : map.values()) {
+         if(count != 9) { //return false if the board doesn't have 0-9
+            return false;
+         }
       }
+      //return false if the map's keys missing any 1-9
+      for(int k = 1; k <= 9; k++) {
+         if(!map.containsKey(k)) {
+            return false;
+         }
+      }
+      return true;
    }
    
-   public String toString() {
-      String result = "My Board:\n\n";
-      for(int row = 0; row < SIZE; row++) {
-         for(int col = 0; col < SIZE; col++) {
-            result += (myBoard[row][col]);
-         }
-         result += ("\n");
-      }
-      return result;
+   public boolean solve() {
+      solve(0, 0, 1);
+      return false;                 
    }
-
+   
+   private boolean solve(int r, int c, int x) {
+      //base case: if it's invalid, it can't be solved
+      if(!isValid()) {
+         return false;
+      }
+      //base case: if it's solved, return true
+      else if(isSolved()) {
+         return true;
+      } else {
+         if(board[r][c] == 0) {
+            board[r][c] = x;
+            if(!isValid()) {          
+               solve(r, c, x + 1);
+            } else if(c < 9) {
+               solve(r, c + 1, x);
+            } else if (c >= 9){
+               solve(r + 1, 0, x);
+            }
+         }                
+      }                    
+      return false;
+   }
+      
+ 
 }
+ 
+ /*
+Checking empty board...passed.
+Checking incomplete, valid board...passed.
+Checking complete, valid board...passed.
+Checking dirty data board...passed.
+Checking row violating board...passed.
+Checking col violating board...passed.
+Checking row&col violating board...passed.
+Checking mini-square violating board...passed.
+**** HORRAY: ALL TESTS PASSED ****x` 
+ */
